6a0d6d3267b029d24cb8b8ccd31f18a79ad39b1d
Drew Cuthbertson <syeoryn@gmail.com>
2014-09-27 19:44:36 -0700

    Implement reportScore

--- roundHandler.js
+++ roundHandler.js
@@ -12,21 +12,29 @@ currentRound.timeRemaining = function(){
 var startNewRound = function(){
   currentRound.card = flashCards.getFlashCard();
   currentRound.startTime = new Date().getTime() + gameOptions.timeOffset;
+  currentRound.status = 'active';
   setTimeout(endRound, currentRound.timeRemaining());
 }
 
 var reportScore = function(message){
-
+  if(currentRound.status === 'inactive') return;
+  if(!currentRound.card.responses) currentRound.responses = {};
+  var userResponse = message.text.toLowerCase();
+  currentRound.responses[message.msisdn] = {
+    response: userResponse,
+    correctAnswer: userResponse === currentRound.card.answer.toLowerCase()
+  }
 }
 
 var endRound = function(){
+  currentRound.status = 'inactive';
   reportRound();
   currentRound.card = null;
   setTimeout(startNewRound, gameOptions.timeBetweenRounds);
 }
 
 var reportRound = function(){
-
+  console.log('reporting round!', currentRound.responses);
 }
 
 var getCurrentRound = function(){
