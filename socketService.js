31c2adb4d18ac4d9ab0be1b20da093e7801cc484
Drew Cuthbertson <syeoryn@gmail.com>
2014-09-28 22:31:13 -0700

    Implement websocket broadcast

--- socketService.js
+++ socketService.js
@@ -1,5 +1,7 @@
 var WebSocketServer = require('ws').Server;
 
+var openConnections = [];
+
 var initializeSocket = function(server, path){
   var options = {
     server: server,
@@ -9,11 +11,28 @@ var initializeSocket = function(server, path){
   var wss = new WebSocketServer(options);
 
   wss.on('connection', function(ws){
+    openConnections.push(ws);
     ws.send('Great success');
   });
 }
 
+var broadcast = function(message, type){
+  var updatedOpenConnections = [];
+  for(var i = 0; i < openConnections.length; i++){
+    var ws= openConnections[i];
+    if(ws.readyState === ws.OPEN){
+      ws.send( JSON.stringify({
+        message: message,
+        type: type
+      }) );
+      updatedOpenConnections.push(ws);
+    }
+  }
+  openConnections = updatedOpenConnections;
+}
+
 module.exports = {
-  initializeSocket: initializeSocket
+  initializeSocket: initializeSocket,
+  broadcast: broadcast
 };
 
